74a1358a270f478f7a016466fe8c736d9d72e25f
alexisjhb <169068450+alexisjhb@users.noreply.github.com>
2024-05-13 14:58:37 +0000

    dia 13 de maio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,18 @@
 public class Main {
   public static void main(String[] args) {
-  System.out.println("##################### ");
-  System.out.println("#Artista: Bruno mars#");
-System.out.println("#####################");
+System.out.println("|-------------------\\ ");
+System.out.println("|*Artista: Bruno mars-\\");
+System.out.println("|----------------------\\");
     
 System.out.println("");
 
-System.out.println("Album : 24k Magic");
-System.out.println("Ano de lançamento: 2016");
-
+System.out.println("-Album : 24k Magic-");
+System.out.println("-Ano de lançamento: 2016-");
+            
 System.out.println("");
 
-System.out.println("")  
+
+
+    
   }
 }      
