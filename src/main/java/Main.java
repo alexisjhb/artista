0a850a3628da07c1b43d92326200e468ad13764d
alexisjhb <169068450+alexisjhb@users.noreply.github.com>
2024-05-13 14:38:12 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+  System.out.println("##################### ");
+  System.out.println("#Artista: Bruno mars#");
+System.out.println("#####################");
+    
+System.out.println("");
+
+System.out.println("Album : 24k Magic");
+System.out.println("Ano de lançamento: 2016");
+
+System.out.println("");
+
+System.out.println("")  
+  }
+}      
